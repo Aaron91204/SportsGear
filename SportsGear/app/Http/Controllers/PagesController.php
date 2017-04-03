c8e18c5863b772af3fa43df91cd679d6523dae51
Mark O'Hare <marko910@hotmail.com>
2017-04-03 18:00:28 +0100

    Updated composer and added PagesController code

--- SportsGear/app/Http/Controllers/PagesController.php
+++ SportsGear/app/Http/Controllers/PagesController.php
@@ -6,8 +6,31 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function home()
-    {
-    
+     public function home(){
+    	return view('pages.home');
+    }
+
+    public function register(){
+    	return view('pages.register');
+    }
+
+    public function login(){
+    	return view('pages.login');
+    }
+
+    public function products(){
+    	return view('pages.products');
+    }
+
+    public function product(){
+    	return view('pages.product');
+    }
+
+    public function cart(){
+    	return view('pages.cart');
+    }
+
+    public function confirmation(){
+    	return view('pages.confirmation');
     }
 }
