70a5c0ecfa56e3cce3a8b2f2b5c859e6dbc0000a
Mark O'Hare <marko910@hotmail.com>
2017-04-10 13:20:53 +0100

    Fixed password error for registering a new user

--- SportsGear/app/User.php
+++ SportsGear/app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'firstname','surname','address', 'postcode','telephone','email', 'username',
+        'firstname','surname','address', 'postcode','telephone','email', 'username', 'password',
     ];
 
     /**
