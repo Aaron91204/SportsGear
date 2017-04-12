87824a6cd9d4de55ced6b99f5440619efada4717
Aaron91204 <aaronnicholl@ymail.com>
2017-04-12 20:52:55 +0100

    Contact Us Form Templete

--- SportsGear/resources/views/home.blade.php
+++ SportsGear/resources/views/home.blade.php
@@ -9,8 +9,69 @@
 
                 <div class="panel-body">
                     You are logged in!
-                    <a href="{{ url('/product') }}">Product</a>
+                    <br>
+                    <br>
+                    Contact Us Form
+                    <br>
+                    If you have any questons for us please feel free to use the following form to contact us
+                    <br>
+                    <br>
+                    <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">First Name</label>
+
+                            <div class="col-md-6">
+                                <input id="firstname" type="text" class="form-control" name="firstname" value="{{ old('firstname') }}" required autofocus>
+
+                                @if ($errors->has('firstname'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('firstname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+
+                        <div class="form-group{{ $errors->has('surname') ? ' has-error' : '' }}">
+                            <label for="surname" class="col-md-4 control-label">Surname</label>
+
+                            <div class="col-md-6">
+                                <input id="surname" type="text" class="form-control" name="surname" value="{{ old('surname') }}" required autofocus>
+
+                                @if ($errors->has('surname'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('surname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="form-group{{ $errors->has('surname') ? ' has-error' : '' }}">
+                            <label for="surname" class="col-md-4 control-label">Please enter your question</label>
+
+                            <div class="col-md-6">
+                                <textarea name="notes" cols="50" rows="10"></textarea>
+
+                                @if ($errors->has('surname'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('surname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+
                 </div>
+                
             </div>
         </div>
     </div>
