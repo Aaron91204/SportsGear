b31f1fc2319bab9f6bc187e00cf5a2aa5ab9bab1
rachel-mulholland <rachmulholland@yahoo.co.uk>
2017-04-12 21:20:54 +0100

    Add files via upload
    Categories page added

---
+++ SportsGear/resources/views/categories.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+
+        @if (session()->has('success_message'))
+            <div class="alert alert-success">
+                {{ session()->get('success_message') }}
+            </div>
+        @endif
+
+        @if (session()->has('error_message'))
+            <div class="alert alert-danger">
+                {{ session()->get('error_message') }}
+            </div>
+        @endif
+
+       <div class="jumbotron text-center clearfix">
+            <h2>Categories</h2>
+            <p>Categories List</p>
+            
+        </div> <!-- end jumbotron -->
+
+        @foreach ($products->chunk(4) as $items)
+            <div class="row">
+                @foreach ($items as $product)
+                    <div class="col-md-3">
+                        <div class="thumbnail">
+                            <div class="caption text-center">
+                                <h3><a href="/">{{ $product->category}}</a></h3>
+                                
+                            </div> <!-- end caption -->
+                        </div> <!-- end thumbnail -->
+                    </div> <!-- end col-md-3 -->
+                @endforeach
+            </div> <!-- end row -->
+        @endforeach
+
+        
+    </div>
+@endsection
